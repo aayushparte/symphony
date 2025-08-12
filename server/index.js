a1a1b86f1c71ee0991137604173d2c605019ac21
Aayush Parte <parteaayush17@gmail.com>
2025-08-12 14:27:25 +0530

    Chnaged port access

--- server/index.js
+++ server/index.js
@@ -20,4 +20,5 @@ app.use('/data',datarouter);
 app.use('/student',studentrouter)
 
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
